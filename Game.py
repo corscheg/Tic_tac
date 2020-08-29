b314f1458ba155d9a1073d99309852b078118c62
Aleksandr <sanbz2007@gmail.com>
2020-08-30 01:57:32 +0300

    Win line works

--- Game.py
+++ Game.py
@@ -2,6 +2,7 @@ class Game:
     def __init__(self):
         self.state = []
         self.player = '0'
+        self.win_line = []
         for i in range(9):
             self.state.append(None)
 
@@ -23,22 +24,34 @@ class Game:
     def get_player(self):
         return self.player
 
+    @property
+    def get_win_line(self):
+        return self.win_line
+
     def win_check(self):
         if (self.state[0] == self.state[1] == self.state[2]) and (self.state[0] is not None):
+            self.win_line = [0, 2]
             return self.state[0]
         elif (self.state[3] == self.state[4] == self.state[5]) and (self.state[3] is not None):
+            self.win_line = [3, 5]
             return self.state[3]
         elif (self.state[6] == self.state[7] == self.state[8]) and (self.state[6] is not None):
+            self.win_line = [6, 8]
             return self.state[6]
         elif (self.state[0] == self.state[3] == self.state[6]) and (self.state[0] is not None):
+            self.win_line = [0, 6]
             return self.state[6]
         elif (self.state[1] == self.state[4] == self.state[7]) and (self.state[1] is not None):
+            self.win_line = [1, 7]
             return self.state[1]
         elif (self.state[2] == self.state[5] == self.state[8]) and (self.state[2] is not None):
+            self.win_line = [2, 8]
             return self.state[2]
         elif (self.state[0] == self.state[4] == self.state[8]) and (self.state[0] is not None):
+            self.win_line = [0, 8]
             return self.state[0]
         elif (self.state[2] == self.state[4] == self.state[6]) and (self.state[2] is not None):
+            self.win_line = [2, 6]
             return self.state[2]
         else:
             s = 0
--- Tic_tac.py
+++ Tic_tac.py
@@ -3,19 +3,27 @@ import pygame
 pygame.init()
 
 
-def drCym(field, symbol):
+def drSym(field, symbol):
     if symbol == '0':
         pict = pygame.image.load('Images/Zero.bmp')
     elif symbol == 'X':
         pict = pygame.image.load('Images/Cross.bmp')
 
     back.blit(pict, BUTTONS[field])
-    pygame.display.update()
+
+
+def drawWinLine(win_line):
+    print(win_line)
+    point1 = BUTTONS[win_line[0]].center
+    point2 = BUTTONS[win_line[1]].center
+    pygame.draw.line(back, RED, point1, point2, 10)
+    pygame.draw.circle(back, RED, point1, 5)
+    pygame.draw.circle(back, RED, point2, 5)
 
 
 def turn(ps):
     if not m.get_occ(ps):
-        drCym(ps, m.get_player)
+        drSym(ps, m.get_player)
     m.move(ps)
 
 
@@ -23,6 +31,7 @@ WIN_WIDTH = 640
 WIN_HEIGHT = 480
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED = (128, 0, 0)
 BUTTONS = (pygame.Rect(20, 268, 112, 112), pygame.Rect(144, 268, 112, 112), pygame.Rect(268, 268, 112, 112), pygame.Rect(20, 144, 112, 112), pygame.Rect(144, 144, 112, 112), pygame.Rect(268, 144, 112, 112), pygame.Rect(20, 20, 112, 112), pygame.Rect(144, 20, 112, 112), pygame.Rect(268, 20, 112, 112))
 FPS = 30
 
@@ -53,6 +62,10 @@ while 1:
                 pygame.display.update()
 
                 if m.win_check():
+                    drawWinLine(m.get_win_line)
+                    sc.blit(back, (WIN_WIDTH // 2 - back.get_width() // 2, WIN_HEIGHT // 2 - back.get_height() // 2))
+                    pygame.display.update()
+                    pygame.time.delay(5000)
                     exit()
 
     pygame.time.delay(1000 // FPS)
